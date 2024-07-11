f3a0074c71295eff16e1ffb3ff85b6a4a3eca493
Maksim <voronovmaksim88@gmail.com>
2024-07-11 16:20:18 +0300

    Мелкие правочки

--- colorama_r1.py
+++ colorama_r1.py
@@ -8,18 +8,18 @@ colorama.init(autoreset=True)
 num = 123  # Задаем числовую переменную
 
 print(Fore.BLUE + str(num))  # Выводим значение переменной голубым цветом
-print(Fore.LIGHTBLUE_EX + str(num))  # Выводим значение переменной светлоголубым цветом
+print(Fore.LIGHTBLUE_EX + str(num))  # Выводим значение переменной светло-голубым цветом
 
 print(Fore.GREEN + str(num))  # Выводим значение переменной зелёным цветом
-print(Fore.LIGHTGREEN_EX + str(num))  # Выводим значение переменной светлозелёным цветом
+print(Fore.LIGHTGREEN_EX + str(num))  # Выводим значение переменной светло-зелёным цветом
 
 print(Fore.RED + str(num))  # Выводим значение переменной красным цветом
-print(Fore.LIGHTRED_EX + str(num))  # Выводим значение переменной светлокрасным цветом
+print(Fore.LIGHTRED_EX + str(num))  # Выводим значение переменной светло-красным цветом
 
 print(Fore.MAGENTA + str(num))  # Выводим значение переменной сиреневым цветом
 print(Fore.LIGHTMAGENTA_EX + str(num))  # Выводим значение переменной сиреневым цветом
 
 print(Fore.YELLOW + str(num))  # Выводим значение переменной жёлтым цветом
-print(Fore.LIGHTYELLOW_EX + Style.BRIGHT + str(num) + " bold")  # Выводим значение переменной светложёлтым цветом
+print(Fore.LIGHTYELLOW_EX + Style.BRIGHT + str(num) + " bold")  # Выводим значение переменной светло-жёлтым цветом
 
 print("white hapy end")
---
+++ map.py
@@ -0,0 +1,16 @@
+# Мэппинг — это функция, которая применяет другую функцию к итерируемому объекту.
+# map(function, iterable, *iterables)¶
+# Return an iterator that applies function to every item of iterable, yielding the results.
+# map возвращает итератор
+
+ls = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+def my_fn(x):
+    return x * 2
+
+
+r = map(my_fn, ls)  # Заметили, что скобок у my_func нет?
+print(r)
+print(type(r))
+print(list(r))  # Выводит список, который содержит итератор
