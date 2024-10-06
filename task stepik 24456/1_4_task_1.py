d39c8c1b95bfd926cbe3a2984862c8aac00e7cc1
voronovmaksim88 <voronovmaksim88@gmail.com>
2024-10-06 18:25:14 +0700

    застрял на 1_4 task_1

--- task stepik 24456/1_4_task_1.py
+++ task stepik 24456/1_4_task_1.py
@@ -1,5 +1,6 @@
 """
-Реализуйте программу, которая будет эмулировать работу с пространствами имен. Необходимо реализовать поддержку создания пространств имен и добавление в них переменных.
+Реализуйте программу, которая будет эмулировать работу с пространствами имен. Необходимо реализовать поддержку создания
+ пространств имен и добавление в них переменных.
 
 В данной задаче у каждого пространства имен есть уникальный текстовый идентификатор – его имя.
 
@@ -7,7 +8,8 @@
 
 create <namespace> <parent> –  создать новое пространство имен с именем <namespace> внутри пространства <parent>
 add <namespace> <var> – добавить в пространство <namespace> переменную <var>
-get <namespace> <var> – получить имя пространства, из которого будет взята переменная <var> при запросе из пространства <namespace>, или None, если такого пространства не существует
+get <namespace> <var> – получить имя пространства, из которого будет взята переменная <var> при запросе из пространства
+ <namespace>, или None, если такого пространства не существует
 Рассмотрим набор запросов
 
 add global a
@@ -15,14 +17,19 @@ create foo global
 add foo b
 create bar foo
 add bar a
-Структура пространств имен описанная данными запросами будет эквивалентна структуре пространств имен, созданной при выполнении данного кода
+Структура пространств имен описанная данными запросами будет эквивалентна структуре пространств имен,
+ созданной при выполнении данного кода
 
 a = 0
 def foo():
   b = 1
   def bar():
     a = 2
-В основном теле программы мы объявляем переменную a, тем самым добавляя ее в пространство global. Далее мы объявляем функцию foo, что влечет за собой создание локального для нее пространства имен внутри пространства global. В нашем случае, это описывается командой create foo global. Далее мы объявляем внутри функции foo функцию bar, тем самым создавая пространство bar внутри пространства foo, и добавляем в bar переменную a.
+В основном теле программы мы объявляем переменную a, тем самым добавляя ее в пространство global.
+ Далее мы объявляем функцию foo,
+  что влечет за собой создание локального для нее пространства имен внутри пространства global.
+  В нашем случае, это описывается командой create foo global. Далее мы объявляем внутри функции foo функцию bar,
+   тем самым создавая пространство bar внутри пространства foo, и добавляем в bar переменную a.
 
 Добавим запросы get к нашим запросам
 
@@ -42,20 +49,25 @@ def foo():
     get(a)
     get(b)
 Результатом запроса get будет имя пространства, из которого будет взята нужная переменная.
-Например, результатом запроса get foo a будет global, потому что в пространстве foo не объявлена переменная a, но в пространстве global, внутри которого находится пространство foo, она объявлена. Аналогично, результатом запроса get bar b будет являться foo, а результатом работы get bar a будет являться bar.
+Например, результатом запроса get foo a будет global, потому что в пространстве foo не объявлена переменная a,
+ но в пространстве global, внутри которого находится пространство foo, она объявлена. Аналогично,
+  результатом запроса get bar b будет являться foo, а результатом работы get bar a будет являться bar.
 
-Результатом get foo c будет являться None, потому что ни в пространстве foo, ни в его внешнем пространстве global не была объявлена переменная с.
+Результатом get foo c будет являться None, потому что ни в пространстве foo,
+ ни в его внешнем пространстве global не была объявлена переменная с.
 
 Более формально, результатом работы get <namespace> <var> является
 
 <namespace>, если в пространстве <namespace> была объявлена переменная <var>
-get <parent> <var> – результат запроса к пространству, внутри которого было создано пространство <namespace>, если переменная не была объявлена
+get <parent> <var> – результат запроса к пространству, внутри которого было создано пространство <namespace>,
+ если переменная не была объявлена
 None, если не существует <parent>, т. е. <namespace> – это global
 Формат входных данных
 В первой строке дано число n (1 ≤ n ≤ 100) – число запросов.
 В каждой из следующих n строк дано по одному запросу.
 Запросы выполняются в порядке, в котором они даны во входных данных.
-Имена пространства имен и имена переменных представляют из себя строки длины не более 10, состоящие из строчных латинских букв.
+Имена пространства имен и имена переменных представляют из себя строки длины не более 10,
+ состоящие из строчных латинских букв.
 
 Формат выходных данных
 Для каждого запроса get выведите в отдельной строке его результат.
@@ -112,7 +124,7 @@ def add(data, name_spase, var):
 
     if isinstance(data, dict):  # Если текущий элемент - словарь
         for key, value in data.items():
-            print(key)  # Выводим ключ
+            #print(key)  # Выводим ключ
             if key == name_spase:
                 data[key].append(var)
             add(value, name_spase, var)  # Рекурсивный вызов для значения
@@ -130,42 +142,77 @@ def get(data, name_spase, var):
 
     if isinstance(data, dict):  # Если текущий элемент - словарь
         for key, value in data.items():
-            print(key)  # Выводим ключ
-            if key == name_spase:
-                data[key].append(var)
-            add(value, name_spase, var)  # Рекурсивный вызов для значения
+            name_spase = key
+            return get(value, name_spase, var)  # Рекурсивный вызов для значения
+
     elif isinstance(data, list):  # Если текущий элемент - список
-        for item in data:
-            add(item, name_spase, var)  # Рекурсивный вызов для каждого элемента списка
+        for i in range(len(data)):
+            if data[i] == var:
+                return "varible " + data[i] + " was finded in namespase " + name_spase
+            if isinstance(data[i], dict):
+                return get(data[i], name_spase, var)  # Рекурсивный вызов для каждого элемента списка
+            if i == len(data)-1:
+                return None
+
+    # elif isinstance(data, str):  # Если текущий элемент - список
+    #     if data == var:
+    #         return "varible " + data + " was finded in namespase " + name_spase
+    #     else:
+    #         return None
+
+
 
 
 # словарь для хранения пространств имен
 # name_space_dict = {'global': [{'foo': [{'f2': []}, {'f3': []}]}, {'foo1': []}]}
 name_space_dict = {'global': []}
 
-create(name_space_dict, 'a', 'global')
-create(name_space_dict, 'b', 'a')
-add(name_space_dict, 'b', 'c')
+# create(name_space_dict, 'a', 'global')
+# create(name_space_dict, 'b', 'a')
+# add(name_space_dict, 'b', 'c')
+
+add(name_space_dict, 'global', 'var1')
+add(name_space_dict, 'global', 'var2')
+add(name_space_dict, 'global', 'var3')
+
+# print(get(name_space_dict, 'global', 'var1'))
+# print(get(name_space_dict, 'global', 'var2'))
+# print(get(name_space_dict, 'global', 'var3'))
+# print(get(name_space_dict, 'global', 'var4'))
+# print()
+
+create(name_space_dict, 'ns1', 'global')
+add(name_space_dict, 'ns1', 'var2')
+print(get(name_space_dict, 'ns1', 'var2'))
+print()
+
+# add(name_space_dict, 'global', 'var3')
+# get(name_space_dict, 'ns1', 'var3')
+#
+# get(name_space_dict, 'ns1', 'var3')
+#
+# get(name_space_dict, 'global', 'var2')
+
 print(name_space_dict)
 
-request_qty = int(input())
-for i in range(request_qty):
-    comand, arg1, arg2 = input().split()
-    if comand == "add":
-        # arg1 - это имя пространства имён
-        name_space_dict[arg1].append(arg2)
-
-    if comand == "create":
-        # arg1 - Это имя пространства имён, которое надо создать.
-        # arg2 - Это имя пространства, внутри которого надо создать.
-        for name_space in name_space_dict:
-            if name_space == arg2:
-                name_space_dict[name_space].append({arg1: []})
-
-    if comand == "get":
-        print(name_space_dict[arg2])
-
-print(name_space_dict)
+# request_qty = int(input())
+# for i in range(request_qty):
+#     comand, arg1, arg2 = input().split()
+#     if comand == "add":
+#         # arg1 - это имя пространства имён
+#         name_space_dict[arg1].append(arg2)
+#
+#     if comand == "create":
+#         # arg1 - Это имя пространства имён, которое надо создать.
+#         # arg2 - Это имя пространства, внутри которого надо создать.
+#         for name_space in name_space_dict:
+#             if name_space == arg2:
+#                 name_space_dict[name_space].append({arg1: []})
+#
+#     if comand == "get":
+#         print(name_space_dict[arg2])
+#
+# print(name_space_dict)
 
 # def print_keys(data):
 #     if isinstance(data, dict):  # Если текущий элемент - словарь
