c69fcfb12c5abac10f27410fe3becf2c67d2b701
Maksim <voronovmaksim88@gmail.com>
2024-10-21 11:29:16 +0700

    add difference_вычитание

---
+++ Data structures/set_множества/difference_вычитание.py
@@ -0,0 +1,7 @@
+playlist_1 = {'Голубой вагон', 'Облака', 'Yesterday', 'Наше лето'}
+playlist_2 = {'Наше лето', 'Голубой вагон', 'Облака'}
+playlist_3 = playlist_1.difference(playlist_2)
+
+print(playlist_1)
+print(playlist_2)
+print(playlist_3)
