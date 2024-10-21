3bf6b3bcb31e2d2faa5446141e67323d72e623e8
Maksim <voronovmaksim88@gmail.com>
2024-10-21 10:29:44 +0700

    add set example

---
+++ Data structures/set_множества/set_union_объединение.py
@@ -0,0 +1,5 @@
+playlist_1 = {'Три белых коня', 'Happy new year', 'Снежинка'}
+playlist_2 = {'Last christmas', 'Снежинка', 'Happy new year'}
+playlist_3 = playlist_1.union(playlist_2)
+
+print(playlist_3)
