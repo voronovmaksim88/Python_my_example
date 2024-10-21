2565c4385389bfe7ee122539180a6577b186fb83
voronovmaksim88 <voronovmaksim88@gmail.com>
2024-10-21 22:12:19 +0700

    dict

--- Data structures/dictionary_словари/Enumerating_a_dictionary_перебор_словаря.py
+++ Data structures/dictionary_словари/Enumerating_a_dictionary_перебор_словаря.py
@@ -1,3 +1,19 @@
 my_dict = {1: "one", 2: "two", 3: "three", 4: "four", 5: "five"}
 for key, value in my_dict.items():
     print(key, value)
+
+favorite_songs = {
+    'Тополиный пух': 'Иванушки international',
+    'Город золотой': 'Аквариум',
+    'Звезда по имени Солнце': 'Кино',
+    'Space Oddity': 'David Bowie',
+    'Рыба': 'Аквариум',
+    'Серенада Трубадура': 'Муслим Магомаев',
+}
+# Извлечём и напечатаем только значения (values) каждого элемента
+for singer in favorite_songs.values():
+    print('Доктор, я больше не могу слушать исполнителя ' + singer)
+
+# А в этом цикле извлечём и напечатаем только ключи (keys) словаря
+for music in favorite_songs.keys():
+    print('Доктор, я каждый день по три раза слушаю песню ' + music)
