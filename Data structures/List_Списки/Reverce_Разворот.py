ac9f689f7e0c13514262d85f0b3b31a0653af769
voronovmaksim88 <voronovmaksim88@gmail.com>
2024-10-27 23:13:24 +0700

    решил задачу на степике

---
+++ Data structures/List_Списки/Reverce_Разворот.py
@@ -0,0 +1,4 @@
+
+my_list = [1, 2, 3, 4, 5]
+my_list.reverse()
+print(my_list)
--- task stepik 24456/1_4_task_1.py
+++ task stepik 24456/1_4_task_1.py
@@ -108,7 +108,7 @@ def create(data, new_name_spase, parent_name_space):
 
     if isinstance(data, dict):  # Если текущий элемент - словарь
         for key, value in data.items():
-            print(key)  # Выводим ключ
+            # print(key)  # Выводим ключ
             if key == parent_name_space:
                 data[key].append({new_name_spase: []})
             create(value, new_name_spase, parent_name_space)  # Рекурсивный вызов для значения
@@ -133,95 +133,30 @@ def add(data, name_spase, var):
             add(item, name_spase, var)  # Рекурсивный вызов для каждого элемента списка
 
 
-def get(data: dict,
-        name_space_fix: str,
-        var_name: str,
-        name_space: str) -> str:
-    def search_in_list(lst, current_ns):
-        for item in lst:
-            if item == var_name:
-                return f"variable {item} was found in namespace: {current_ns}"
-            if isinstance(item, dict):
-                result = search_in_dict(item)
-                if result:
-                    return result
-        return None
-
-    def search_in_dict(d):
-        for key, value in d.items():
-            # print(f"ищем в словаре {key}")
-            if isinstance(value, list):
-                # print(f"ищем в namespace {key}")
-                result = search_in_list(value, key)
-                if result:
-                    return result
-        return None
-
-    if isinstance(data, dict):
-        return search_in_dict(data)
-    elif isinstance(data, list):
-        return search_in_list(data, name_space)
-
-    return "Variable not found"
-
 
 # словарь для хранения пространств имен
 # name_space_dict = {'global': [{'foo': [{'f2': []}, {'f3': []}]}, {'foo1': []}]}
 name_space_dict = {'global': []}
-
-add(name_space_dict, 'global', 'v_01')
-add(name_space_dict, 'global', 'v_02')
-add(name_space_dict, 'global', 'v_03')
-
-create(name_space_dict, 'ns1', 'global')
-add(name_space_dict, 'global', 'v_04')
-add(name_space_dict, 'ns1', 'v_10')
-add(name_space_dict, 'ns1', 'v_11')
-
-create(name_space_dict, 'ns2', 'ns1')
-add(name_space_dict, 'ns2', 'v_20')
-add(name_space_dict, 'ns2', 'v_21')
-
-create(name_space_dict, 'ns3', 'ns2')
-
-print("get result:", get(name_space_dict, 'global', 'v_20', "global"))
-print()
-
-print(name_space_dict)
-print()
-
-
-# request_qty = int(input())
-# for i in range(request_qty):
-#     command, arg1, arg2 = input().split()
-#     if command == "add":
-#         # arg1 - это имя пространства имён
-#         name_space_dict[arg1].append(arg2)
 #
-#     if command == "create":
-#         # arg1 - Это имя пространства имён, которое надо создать.
-#         # arg2 - Это имя пространства, внутри которого надо создать.
-#         for name_space in name_space_dict:
-#             if name_space == arg2:
-#                 name_space_dict[name_space].append({arg1: []})
+# add(name_space_dict, 'global', 'v_01')
+# add(name_space_dict, 'global', 'v_02')
+# add(name_space_dict, 'global', 'v_03')
 #
-#     if command == "get":
-#         print(name_space_dict[arg2])
+# create(name_space_dict, 'ns1', 'global')
+# add(name_space_dict, 'global', 'v_04')
+# add(name_space_dict, 'ns1', 'v_10')
+# add(name_space_dict, 'ns1', 'v_11')
+#
+# create(name_space_dict, 'ns2', 'ns1')
+# add(name_space_dict, 'ns2', 'v_20')
+# add(name_space_dict, 'ns2', 'v_21')
+#
+# create(name_space_dict, 'ns3', 'ns2')
 #
 # print(name_space_dict)
+# print()
 
-# def print_keys(data):
-#     if isinstance(data, dict):  # Если текущий элемент - словарь
-#         for key, value in data.items():
-#             print(key)  # Выводим ключ
-#             print_keys(value)  # Рекурсивный вызов для значения
-#     elif isinstance(data, list):  # Если текущий элемент - список
-#         for item in data:
-#             print_keys(item)  # Рекурсивный вызов для каждого элемента списка
-#
-# # Пример использования
-# example = {"G": [{"f": [{"f1": []}]}]}
-# print_keys(example)
+
 
 
 def print_name_space(name_space_dict_in: dict, tab="", i=0):
@@ -236,17 +171,44 @@ def print_name_space(name_space_dict_in: dict, tab="", i=0):
             print_name_space(item, tab + "  ", i)
 
 
-print_name_space(name_space_dict)
 
+def search_in_name_space(data: dict, namespace: str, variable: str) -> bool:
+    def find_namespace(current_data):
+        # Если текущий элемент словарь
+        if isinstance(current_data, dict):
+            # Перебираем ключи и значения словаря
+            for key, value in current_data.items():
+                # Если нашли искомое пространство имен
+                if key == namespace:
+                    # Проверяем, есть ли переменная в этом пространстве
+                    if isinstance(value, list):
+                        return variable in value
+                    return False
+                # Рекурсивно ищем в значении словаря
+                result = find_namespace(value)
+                if result is not None:  # Если нашли результат (True или False)
+                    return result
 
-def search_in_list(lst: list, var_name: str, current_ns: str):
-    """
-    Эта функция проеяет есть ли переменная в namespace
-    """
-    for item in lst:
-        if item == var_name:
-            return f"variable {item} was found in namespace: {current_ns}"
-    return None
+        # Если текущий элемент список
+        elif isinstance(current_data, list):
+            # Перебираем элементы списка
+            for item in current_data:
+                # Если элемент - словарь, ищем в нем
+                if isinstance(item, dict):
+                    result = find_namespace(item)
+                    if result is not None:  # Если нашли результат
+                        return result
+
+        return None
+
+    # Специальная обработка для корневого пространства 'global'
+    if namespace == 'global':
+        if isinstance(data.get('global'), list):
+            return variable in data['global']
+        return False
+
+    result = find_namespace(data)
+    return result if result is not None else False
 
 
 def create_pass_name_space(data: dict, ns: str) -> list:
@@ -279,5 +241,52 @@ def create_pass_name_space(data: dict, ns: str) -> list:
     return result if result else []
 
 
-print(search_in_list(name_space_dict["global"], "v_01", "global"))
-print('путь', create_pass_name_space(name_space_dict, "ns3"))
+def get(data: dict,
+        name_space_fix: str,
+        var_name: str) -> str:
+    path = create_pass_name_space(data, name_space_fix)
+    path.reverse()
+    for ns_i in path:
+        in_ns_i = search_in_name_space(name_space_dict, ns_i, var_name)
+        if in_ns_i:
+            return ns_i
+    return "None"
+
+
+request_qty = int(input())
+commands = []
+for i in range(request_qty):
+    commands.append(input())
+
+# print(commands)
+
+for com in commands:
+    command, arg1, arg2 = com.split()
+    if command == "add":
+        # arg1 - это имя пространства имён
+        # name_space_dict[arg1].append(arg2)
+        add(name_space_dict, name_spase=arg1, var=arg2)
+
+    if command == "create":
+        # arg1 - Это имя пространства имён, которое надо создать.
+        # arg2 - Это имя пространства, внутри которого надо создать.
+        # for name_space in name_space_dict:
+        #     if name_space == arg2:
+        #         name_space_dict[name_space].append({arg1: []})
+        create(name_space_dict, new_name_spase=arg1, parent_name_space=arg2)
+
+    if command == "get":
+        # print(name_space_dict[arg2])
+        # arg1 -  <namespace>
+        # arg2 -  <var>
+        print(get(name_space_dict, name_space_fix=arg1, var_name=arg2))
+
+# print(name_space_dict)
+
+# print()
+# print_name_space(name_space_dict)
+# print(search_in_name_space(name_space_dict, "global", "v_04"))
+# print('путь', create_pass_name_space(name_space_dict, "ns1"))
+# print("get result:", get(name_space_dict, 'ns1', 'v_04'))
+# print()
+
