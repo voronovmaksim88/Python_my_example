493cd522b55cea499b9f52ec8898f5b25ddb9ac4
Maksim <voronovmaksim88@gmail.com>
2024-11-15 16:28:32 +0700

    должен лог запросов лететь

--- modbus_rtu_test/main.py
+++ modbus_rtu_test/main.py
@@ -1,26 +1,26 @@
-"""
-Читаем год выпуска прошивки комбайна
-"""
-
 import minimalmodbus
 import serial
 import time
 
 # Настройка подключения
-instrument = minimalmodbus.Instrument('COM7', 1)  # COM-порт и адрес устройства
+instrument = minimalmodbus.Instrument('COM1', 1)  # COM-порт и адрес устройства
 instrument.serial.baudrate = 9600  # Скорость передачи
 instrument.serial.parity = serial.PARITY_EVEN  # Четность
 instrument.serial.stopbits = 2  # Стоп-биты
 instrument.serial.bytesize = 8  # Биты данных
 instrument.mode = minimalmodbus.MODE_RTU  # Режим RTU
 
+# Включаем режим отладки
+instrument.debug = True
+minimalmodbus._print_out = True
+
 # Адрес регистра в шестнадцатеричном формате
-BUILD_Y = 0x0109
+register_address = 0x0109
 
 while True:
     try:
         # Чтение значения регистра (функция 3 - Read Holding Registers)
-        value = instrument.read_register(BUILD_Y,
+        value = instrument.read_register(register_address,
                                          functioncode=3,
                                          signed=False)  # unsigned int16
 
