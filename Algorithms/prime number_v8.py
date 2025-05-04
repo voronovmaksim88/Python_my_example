beb43076c367994d701317c56a008f6a95e7fea1
MaksimHome <voronovmaksim88@gmail.com>
2025-05-04 18:29:07 +0700

    мелкие правки

--- Algorithms/prime number_v8.py
+++ Algorithms/prime number_v8.py
@@ -10,7 +10,7 @@
 import time
 
 start_time = time.time()
-i = 10_000_000_000
+i = 1_000_000_000
 print("начинаем поиск простых чисел с ", i)
 stop = ''
 while True:
@@ -24,6 +24,4 @@ while True:
             t = round((time.time() - start_time), 1)
             print("потрачено", t, "сек")
             start_time = time.time()
-            # stop = input("Введите Y  для завершения ")
-    #  print(i)
-# print("На поиск затрачено", b, "секунд")
+
