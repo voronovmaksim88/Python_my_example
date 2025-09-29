8046dfbeab706ad507d49f2d71bce0c2f487af39
voronovmaksim88 <voronovmaksim88@gmail.com>
2025-09-29 20:14:20 +0700

    правим баги с Мироном

--- Basic concepts/addition.py
+++ Basic concepts/addition.py
@@ -1,3 +1,4 @@
+
 # сложение строк
 num1 = input("enter first str ")
 num2 = input("enter second str ")
@@ -17,8 +18,8 @@ res = num1 + num2
 print("num1+num2 = ", res, "\n")
 
 # сложение логических переменных
-num1 = bool(input("enter first bool var "))
-num2 = bool(input("enter second bool var "))
+num1 = bool(int(input("погода хорошая ? 1-да. 0-нет ")))
+num2 = bool(int(input("свободное время  есть ? 1-да 0-нет")))
 res = num1 and num2
-print("num1 and num2 = ", res, "\n")
+print("Идём гулять ? ", res, "\n")
 input("для завершения нажмите enter")
