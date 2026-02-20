3f5d112c632ec50f693e8da903d0d3faa5c8ab59
voronovmaksim88 <voronovmaksim88@gmail.com>
2026-02-20 16:26:59 +0700

    добавил пару задач которые решал для Мирона. на память.

---
+++ zadachamirona_2/main.py
@@ -0,0 +1,31 @@
+"""
+Задача: Найти количество пятизначных нечётных чисел, состоящих из различных цифр,
+ в десятичной записи которых нет цифр 2, 4, 7, 9.
+ У скольки из них третья цифра является наибольшей
+"""
+
+qty = 0
+all_num=[]
+for e in [1, 3, 5]:
+    for d in [1, 3, 5, 6, 8, 0]:
+        for c in [1, 3, 5, 6, 8, 0]:
+            for b in [1, 3, 5, 6, 8, 0]:
+                for a in [1, 3, 5, 6, 8]:
+                    if (e != d and e != c and e != b and e != a and
+                            d != c and d != b and d != a and
+                            c != b and c != a and
+                            b != a and
+                    c>a and c>b and c>e and c>d):
+                        qty = qty + 1
+                        print(a, b, c, d, e)
+                        # Добавляем число в список
+                        num = int(str(a) + str(b) + str(c) + str(d) + str(e))
+                        all_num.append(num)
+
+print()
+print('qty', qty)
+print()
+
+all_num.sort()
+for i in all_num:
+    print(i)
